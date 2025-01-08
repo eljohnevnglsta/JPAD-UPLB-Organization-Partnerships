d67195e16751ef387fcf64e70241ba1395a592b1
eljohnevnglsta <112616636+eljohnevnglsta@users.noreply.github.com>
2025-01-08 10:55:17 +0800

    feat: added event controller

--- backend/router.js
+++ backend/router.js
@@ -1,4 +1,5 @@
 import { registerAccount, loginAccount, logoutAccount, getAccount, updateAccount } from "./controllers/accounts.js";
+import { createEvent, getEventsByPublisher, getAllEvents, getEventById, updateEvent, deleteEvent } from "./controllers/event.js";
 
 // Routes for the database
 // To fetch data from the database, we need to create routes that will be used to access the data.
@@ -11,6 +12,14 @@ const router = (app) => {
     // TODO: Add middleware for authentication
     app.post('/account/update', updateAccount);
     app.get('/account/get', getAccount);
+
+    app.post('/event/create', createEvent);
+    app.get('/event/get/all', getAllEvents);
+    app.get('/event/get/id', getEventById);
+    app.post('/event/update', updateEvent);
+    app.post('/event/delete', deleteEvent);
+    app.get('/event/get/all/publisher', getEventsByPublisher);
+
     
 }
 
