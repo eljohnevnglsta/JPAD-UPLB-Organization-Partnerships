ab38d0546e537143d272c914e24f790790eadc04
eljohnevnglsta <112616636+eljohnevnglsta@users.noreply.github.com>
2025-01-08 12:08:32 +0800

    feat: added reports controller

--- backend/router.js
+++ backend/router.js
@@ -1,6 +1,8 @@
 import { registerAccount, loginAccount, logoutAccount, getAccount, updateAccount } from "./controllers/accounts.js";
 import { createEvent, getEventsByPublisher, getAllEvents, getEventById, updateEvent, deleteEvent } from "./controllers/event.js";
-
+import { createAnnouncement, getAnnouncementById, getAllAnnouncements, getAnnouncementsByOrg, updateAnnouncement, deleteAnnouncement } from "./controllers/announcement.js";
+import { createRequest, getRequestsByInvitee, getRequestsByPublisher,  getRequestById,  getAllRequests, updateRequest } from "./controllers/request.js";
+import { createReport, getReportById, getAllReports, getReportsByOrg, getReportsByReporter, updateReport } from "./controllers/reports.js";
 // Routes for the database
 // To fetch data from the database, we need to create routes that will be used to access the data.
 // For example: Use the path "localhost:3000/account/register" to register an account.
@@ -19,7 +21,26 @@ const router = (app) => {
     app.post('/event/delete', deleteEvent);
     app.get('/event/get/all/publisher', getEventsByPublisher);
 
-    
+
+    app.post('/announcement/create', createAnnouncement);
+    app.get('/announcement/get/id', getAnnouncementById);
+    app.get('/announcement/get/all', getAllAnnouncements);
+    app.get('/announcement/get/all/org', getAnnouncementsByOrg);
+    app.post('/announcement/update', updateAnnouncement);
+    app.post('/announcement/delete', deleteAnnouncement);
+
+    app.post('/request/create', createRequest);
+    app.get('/request/get/all', getAllRequests);
+    app.get('/request/get/id', getRequestById);
+    app.get('/request/get/all/publisher', getRequestsByPublisher);
+    app.get('/request/get/all/invitee', getRequestsByInvitee);
+    app.post('/request/update', updateRequest);
+
+    app.post('/report/create', createReport);
+    app.get('/report/get/id', getReportById);
+    app.get('/report/get/all', getAllReports);
+    app.get('/report/get/all/org', getReportsByOrg);
+    app.get('/report/get/all/reporter', getReportsByReporter);
 }
 
 export default router;
