4be332e4c0e42eac9d9efcfecfeb9320fd420f59
eljohnevnglsta <112616636+eljohnevnglsta@users.noreply.github.com>
2025-01-13 18:16:56 +0800

    chore: setup tailwindcss

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Profile from './pages/Profile.jsx'
+import Management from './pages/Management.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Profile />
+    {/* <Profile /> */}
+    <Management />
   </StrictMode>,
 )
---
+++ frontend/src/pages/Management.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Management() {
+    return (
+    <h1 className="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+    );  
+}
